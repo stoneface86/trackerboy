0f3ba1c7b76a03a4d018d9a32405bbda02b3b24a
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2019-06-08 21:49:22 -0400

    add new typedefs and functions to gbsynth.h

--- include/gbsynth.h
+++ include/gbsynth.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
 ** File: include/gbsynth.h
 **
 ** Author: stoneface86
@@ -7,4 +7,135 @@
 
 #pragma once
 
+#include <stdbool.h>
+#include <stdint.h>
 
+typedef enum GbsErr {
+    GBS_E_NONE = 0,         // no error
+    GBS_E_MEM,              // out of memory
+    GBS_E_WRONG_CHANNEL,    // setting cannot be applied to given channel
+    GBS_E_PARAMETER         // parameter out of range
+} GbsErr;
+
+typedef enum GbsChType {
+	GBS_CH1,  // square-wave with sweep, envelope
+	GBS_CH2,  // square-wave with envelope
+	GBS_CH3,  // wave playback
+	GBS_CH4   // white noise generator
+} GbsChType;
+
+typedef enum GbsDuty {
+    GBS_DUTY_125 = 0,	// 12.5%
+    GBS_DUTY_25 = 1,	// 25%
+    GBS_DUTY_50 = 2,	// 50%
+    GBS_DUTY_75 = 3		// 75%
+} GbsDuty;
+
+typedef enum GbsEnvMode {
+    GBS_ENV_ATTENUATE = 0,
+    GBS_ENV_AMPLIFY = 1,
+} GbsEnvMode;
+
+typedef enum GbsSweepMode {
+    GBS_SWEEP_ADDITION = 0,
+    GBS_SWEEP_SUBTRACTION = 1
+} GbsSweepMode;
+
+typedef enum GbsWaveOutput {
+	GBS_WAVE_MUTE    = 0,
+	GBS_WAVE_WHOLE   = 1,
+	GBS_WAVE_HALF    = 2,
+	GBS_WAVE_QUARTER = 3
+} GbsWaveOutput;
+
+typedef struct GbsCh1Reg {
+    uint8_t nr10;
+    uint8_t nr11;
+    uint8_t nr12;
+    uint8_t nr13;
+    uint8_t nr14;
+} GbsCh1Reg;
+
+typedef struct GbsCh2Reg {
+    uint8_t nr21;
+    uint8_t nr22;
+    uint8_t nr23;
+    uint8_t nr24;
+} GbsCh2Reg;
+
+typedef struct GbsCh3Reg {
+    uint8_t nr30;
+    uint8_t nr31;
+    uint8_t nr32;
+    uint8_t nr33;
+    uint8_t nr34;
+} GbsCh3Reg;
+
+typedef struct GbsCh3Reg {
+    uint8_t nr41;
+    uint8_t nr42;
+    uint8_t nr43;
+    uint8_t nr44;
+} GbsCh4Reg;
+
+typedef struct GbsSynth GbsSynth;
+
+
+GbsErr gbs_ch1_registers(GbsSynth *synth, GbsCh1Reg *reg);
+
+GbsErr gbs_ch2_registers(GbsSynth *synth, GbsCh2Reg *reg);
+
+GbsErr gbs_ch3_registers(GbsSynth *synth, GbsCh3Reg *reg);
+
+GbsErr gbs_ch4_registers(GbsSynth *synth, GbsCh4Reg *reg);
+
+
+
+// ========================================================
+// Synthesizer settings
+// ========================================================
+
+//
+// Channel 1 settings
+//
+GbsErr gbs_ch1_setSweepTime(GbsSynth *synth, uint8_t ts);
+
+GbsErr gbs_ch1_setSweepType(GbsSynth *synth, uint8_t addition);
+
+GbsErr gbs_ch1_setSweepShift(GbsSynth *synth, uint8_t n);
+
+//
+// Channel 3 settings
+//
+GbsErr gbs_ch3_setEnable(GbsSynth *synth, bool enabled);
+
+GbsErr gbs_ch3_setWaveform(GbsSynth *synth, uint8_t waveform[16]);
+
+
+//
+// general settings
+//
+GbsErr gbs_setDuty(GbsSynth *synth, GbsChType chan, GbsDuty duty);
+
+GbsErr gbs_setLength(GbsSynth *synth, GbsChType chan, uint8_t length);
+
+GbsErr gbs_setEnvSteps(GbsSynth *synth, GbsChType chan, uint8_t steps);
+
+GbsErr gbs_setEnvMode(GbsSynth *synth, GbsChType chan, GbsEnvMode mode);
+
+GbsErr gbs_setEnvLength(GbsSynth *synth, GbsChType chan, uint8_t length);
+
+GbsErr gbs_setFreq(GbsSynth *synth, GbsChType chan, uint16_t freq);
+
+
+//
+// synth
+//
+
+GbsErr gbs_init(GbsSynth **synth, float samplingRate);
+
+GbsErr gbs_deinit(GbsSynth *synth);
+
+float gbs_currentSample(GbsSynth *synth, GbsChType chan);
+
+GbsErr gbs_tick(GbsSynth *synth);
--- src/gbsynth.c
+++ src/gbsynth.c
@@ -1,7 +1,105 @@
-/*
+﻿/*
 ** File: src/gbsynth.c
 **
 ** Author: stoneface86
 */
 
 #include "gbsynth.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+#define NULLCHECK(var) assert(var != NULL)
+
+typedef struct SquareOsc {
+    unsigned ticksPerDuty;
+    unsigned ticksPerPeriod;
+    unsigned ticksPerEnvStep;
+    unsigned envSteps;
+    unsigned ticks;
+} SquareOsc;
+
+struct GbsSynth {
+    float samplingRate;
+    GbsCh1Reg ch1;
+    GbsCh2Reg ch2;
+    GbsCh3Reg ch3;
+    GbsCh4Reg ch4;
+    float currentSamples[4];
+
+
+};
+
+GbsErr gbs_init(GbsSynth **synthVar, float samplingRate) {
+    NULLCHECK(synthVar);
+    GbsSynth *synth = (GbsSynth*)malloc(sizeof(struct GbsSynth));
+    if (synth == NULL) {
+        return GBS_E_MEM;
+    }
+    synth->samplingRate = samplingRate;
+
+
+    *synthVar = synth;
+    return GBS_E_NONE;
+}
+
+GbsErr gbs_deinit(GbsSynth *synth) {
+    NULLCHECK(synth);
+    free(synth);
+    return GBS_E_NONE;
+}
+
+float gbs_currentSample(GbsSynth *synth, GbsChType chan) {
+    NULLCHECK(synth);
+    return synth->currentSamples[chan];
+}
+
+GbsErr gbs_ch1_registers(GbsSynth *synth, GbsCh1Reg *reg) {
+    NULLCHECK(synth);
+    NULLCHECK(reg);
+    *reg = synth->ch1;
+    return GBS_E_NONE;
+}
+
+//void gbs_decodeCh2(GbsCh2 *ch2, GbsCh2Reg *reg) {
+//	ch2->duty = (GbsDuty)(reg->nr21 >> 6);
+//	ch2->length = reg->nr21 & 0x3f;
+//	ch2->envSteps = reg->nr22 >> 4;
+//	ch2->envDirection = (GbsEnvMode)((reg->nr22 >> 3) & 0x1);
+//	ch2->envLength = reg->nr22 & 0x7;
+//	ch2->frequency = ((reg->nr24 & 0x7) << 8) | reg->nr23;
+//}
+//
+//void gbs_encodeCh2(GbsCh2 *ch2, GbsCh2Reg *reg) {
+//    // NR21
+//    // bits 0-5: sound length
+//    // bits 6-7: duty
+//    reg->nr21 = ((ch2->duty & 3) << 6) |
+//                (ch2->length & 0x3f);
+//    // NR22
+//    // bits 0-2: envelope step length
+//    // bit    3: envelope direction
+//    // bits 4-7: enevelop steps
+//    reg->nr22 = ((ch2->envSteps & 0xF) << 4) |
+//                (ch2->envDirection << 3) |
+//                (ch2->envLength & 0x7);
+//
+//    // NR23
+//    // bits 0-7: lower 8 bits of the frequency data
+//    reg->nr23 = ch2->frequency & 0xFF;
+//
+//    // NR24
+//    // bits 0-3: higher 3 bits of the frequency data
+//    reg->nr24 = (ch2->frequency >> 8) & 0x7;
+//}
+//
+//void gbs_setEnvSteps(GbsSynth *synth, GbsChType chan, uint8_t steps) {
+//	switch (chan) {
+//		case GBS_CH_SOUND1:
+//			break;
+//		case GBS_CH_SOUND2:
+//			break;
+//		default:
+//			return -1;
+//	}
+//}
