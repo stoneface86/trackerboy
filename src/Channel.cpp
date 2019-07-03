8734ea5169fcab8915e7e84f0a1b0935a3eff933
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2019-07-03 12:08:12 -0400

    4-bit samples are now converted to floats between -1.0 and 1.0

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -5,21 +5,40 @@
 
 namespace gbsynth {
 
+    static const float SAMPLE_TABLE[] = {
+        -1.0f,
+        -7.0f / 8.0f,
+        -6.0f / 8.0f,
+        -5.0f / 8.0f,
+        -4.0f / 8.0f,
+        -3.0f / 8.0f,
+        -2.0f / 8.0f,
+        -1.0f / 8.0f,
+         0.0f,
+         1.0f / 7.0f,
+         2.0f / 7.0f,
+         3.0f / 7.0f,
+         4.0f / 7.0f,
+         5.0f / 7.0f,
+         6.0f / 7.0f,
+         1.0f
+    };
+
     Channel::Channel() {
         lengthCounter = 0;
-        currentSample = SAMPLE_MIN;
+        currentSample = SAMPLE_GND;
         length = 0;
         continuous = true;
         enabled = true;
     }
 
     uint8_t Channel::getCurrentSample() {
-        return enabled ? currentSample : SAMPLE_MIN;
+        return enabled ? currentSample : SAMPLE_GND;
     }
 
     float Channel::getCurrentVolume() {
         // convert current sample to a value between 0.0f and 1.0f
-        return enabled ? (currentSample / (float)SAMPLE_MAX) : 0.0f;
+        return enabled ? SAMPLE_TABLE[currentSample] : 0.0f;
     }
 
     void Channel::setLength(uint8_t length) {
@@ -46,7 +65,7 @@ namespace gbsynth {
     void Channel::reset() {
         lengthCounter = length;
         enabled = true;
-        currentSample = SAMPLE_MIN;
+        currentSample = SAMPLE_GND;
     }
 
     void Channel::step(unsigned cycles) {
