6bc87ebd61e069395986561b132155081bb966ad
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2019-11-18 18:40:18 -0500

    Add order class unit tests now get compiled into one executable

---
+++ include/trackerboy/song/Order.hpp
@@ -0,0 +1,53 @@
+
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+
+namespace trackerboy {
+
+
+class Order {
+
+public:
+
+    class Sequencer {
+
+    public:
+
+        Sequencer() = default;
+        Sequencer(Order &order);
+        Sequencer(Order &order, size_t loopFor);
+
+        bool hasNext();
+
+        uint8_t next();
+        
+    private:
+        static constexpr size_t INFINITE_LOOP = -1;
+
+        void init(Order &order);
+
+        uint8_t *mPtr;
+        size_t mLoopCounter;
+        uint8_t *mEnd;
+        uint8_t *mLoop;
+    };
+
+    Order();
+
+    std::vector<uint8_t>& indexVec();
+
+    void setLoop(uint8_t index);
+
+    void removeLoop();
+
+private:
+
+    std::vector<uint8_t> mIndexVec;
+    bool mLoops;
+    uint8_t mLoopIndex;
+};
+
+}
---
+++ libtrackerboy/song/Order.cpp
@@ -0,0 +1,72 @@
+
+#include "trackerboy/song/Order.hpp"
+
+#include <exception>
+
+
+namespace trackerboy {
+
+
+Order::Order() :
+    mLoops(false),
+    mLoopIndex(0)
+{
+}
+
+std::vector<uint8_t>& Order::indexVec() {
+    return mIndexVec;
+}
+
+void Order::setLoop(uint8_t index) {
+    if (index >= mIndexVec.size()) {
+        throw std::out_of_range("loop index outside of order");
+    }
+
+    mLoopIndex = index;
+    mLoops = true;
+}
+
+void Order::removeLoop() {
+    mLoops = false;
+}
+
+Order::Sequencer::Sequencer(Order &order) :
+    mLoopCounter(INFINITE_LOOP)
+{
+    init(order);
+}
+
+Order::Sequencer::Sequencer(Order &order, size_t loopFor) :
+    mLoopCounter(loopFor)
+{
+    init(order);
+}
+
+void Order::Sequencer::init(Order &order) {
+    mPtr = order.mIndexVec.data();
+    mEnd = mPtr + order.mIndexVec.size();
+    mLoop = (order.mLoops) ? mPtr + order.mLoopIndex : nullptr;
+}
+
+bool Order::Sequencer::hasNext() {
+    return mPtr != mEnd;
+}
+
+uint8_t Order::Sequencer::next() {
+    uint8_t val = *mPtr++;
+
+    // loop if a loop point was set and the counter is not 0
+    if (mLoop != nullptr && mLoopCounter) {
+        if (mPtr == mEnd) {
+            mPtr = mLoop;
+            if (mLoopCounter != INFINITE_LOOP) {
+                --mLoopCounter;
+            }
+        }
+    }
+
+    return val;
+}
+
+
+}
