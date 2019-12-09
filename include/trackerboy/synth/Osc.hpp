4c1594b8dbe945d83db78ec1160c4fb44029e89c
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2019-12-09 02:43:33 -0500

    Osc is now a base class and no longer templated started work on band-limited synthesis

--- include/trackerboy/synth/Osc.hpp
+++ include/trackerboy/synth/Osc.hpp
@@ -2,48 +2,62 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 namespace trackerboy {
 
-template <unsigned multiplier, size_t segments>
 class Osc {
 
 public:
-    Osc(float samplingRate);
+
+    Osc(float samplingRate, size_t multiplier, size_t waveformSize);
 
     void reset();
 
     void setFrequency(uint16_t frequency);
 
-    void setWaveform(uint8_t waveform[segments / 2]);
-
     void generate(int16_t samples[], size_t nsamples);
+ 
+protected:
+    struct Delta {
+        int16_t change; // the change in volume
+        float time;     // time, in samples, when this delta occurs
+        float duration; // duration of the transition, in samples
+    };
+
+    // waveform as amplitude changes (deltas)
+    // any nonzero value indicates a transition, which will have a bandlimited step
+    std::vector<Delta> mDeltaBuf;
 
 private:
 
-    // if speed is a concern, use fixed point instead of floats
+    void fillPeriod();
+
+    const size_t mWaveformSize;
+    const size_t mMultiplier;
 
     // samplingRate / gameboy clock rate
     float mFactor;
 
-    // number of samples per waveform segment
-    float mSamplesPerSegment;
+    // number of samples per waveform transition (delta)
+    float mSamplesPerDelta;
 
-    float mSampleCounter; // samples for the current segment to make
-    
-    uint8_t mCurrentSegment;
-    // one byte in the waveform is two segments
-    uint8_t mCurrentDoubleSegment;
-    uint8_t mWaveform[segments / 2];
-    uint8_t mWaveIndex;
+    // mSamplesPerDelta * mWaveformSize
+    float mSamplesPerPeriod;
 
-    int16_t mPrevious;
+    // counter used for timing purposes
+    float mSampleCounter;
+
+
+    int16_t mPrevious; // last sample generated
+
+    // set by fillPeriod()
+    std::vector<int16_t> mPeriodBuf;
+    bool mPeriodReady;
+
+
 
 };
-
-
-using PulseOsc = Osc<4, 8>;
-using WaveOsc = Osc<2, 32>;
 
 
 }
