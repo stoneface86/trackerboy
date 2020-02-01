2038f88601800d15383d003256334c7bc5b07a8a
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2020-02-01 17:16:00 -0500

    normalize line endings to LF add .editorconfig file

--- CompilerOverrides.cmake
+++ CompilerOverrides.cmake
@@ -1,5 +1,5 @@
 
 if (MSVC)
-	# remove default /W3 compiler flag
-	string (REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
+    # remove default /W3 compiler flag
+    string (REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
 endif ()
