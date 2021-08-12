9eab070ca8336b0be77b8f7dac85f7d4804b931d
stoneface <44489054+stoneface86@users.noreply.github.com>
2021-08-11 23:45:03 -0400

    back to using a submodule for miniaudio

--- updateminiaudio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/mackron/miniaudio/master/extras/miniaudio_split/miniaudio.h -O include/miniaudio.h
-wget https://raw.githubusercontent.com/mackron/miniaudio/master/extras/miniaudio_split/miniaudio.c -O external/miniaudio.c
