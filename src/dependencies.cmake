695c42f66927caaf15bd8e887cfdce515ecd46bd
stoneface <44489054+stoneface86@users.noreply.github.com>
2022-11-10 11:48:22 -0500

    include miniaudio source code  - removes the need for FetchContent

--- src/dependencies.cmake
+++ src/dependencies.cmake
@@ -1,12 +1,5 @@
 # declare dependencies here, so that CI can use this file's contents as a cache key
 
-
-FetchContent_Declare(
-    miniaudio
-    GIT_REPOSITORY https://github.com/stoneface86/miniaudio-cmake.git
-    GIT_TAG dadfa05c8e189e8336f6441206c3f08d6a52f750 # version 0.10.42
-)
-
 FetchContent_Declare(
     libtrackerboy
     GIT_REPOSITORY https://github.com/stoneface86/libtrackerboy.git
